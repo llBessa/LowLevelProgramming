e014de51e0363ae95a5a8baf497022377fba5622
Lucas Bessa <lucas.bessafp72@gmail.com>
2022-01-10 17:23:20 -0400

    adição do print_rax e comentarios no hello world

--- helloworld/hello.asm
+++ helloworld/hello.asm
@@ -5,12 +5,12 @@ message: db 'hello, world!', 10
 
 section .text
 _start:
-    mov rax, 1      
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 14
-    syscall
+    mov rax, 1          ; numero da chamada de sistema "write" é salvo em rax
+    mov rdi, 1          ; argumento rdi que indica onde escrever
+    mov rsi, message    ; argumento rsi que indica onde a string "começa"
+    mov rdx, 14         ; argumento rdx que indica quantos bytes devem ser escritos
+    syscall             ; chamada de sistema
 
-    mov rax, 60
-    xor rdi, rdi
+    mov rax, 60         ; numero da chamada de sistema "exit" é salvo em rax
+    xor rdi, rdi        
     syscall
